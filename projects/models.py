b1ecc7dd3d33ecd4078a1c5b628c6693c578d7c6
Jerrell Boyer <jerrellbb@yahoo.co.uk>
2024-01-11 17:13:24 +0000

    created projects model

--- projects/models.py
+++ projects/models.py
@@ -7,3 +7,12 @@ class Project(models.Model):
   start_date = models.DateField(null=True, blank=True)
   end_date = models.DateField(null=True, blank=True)
   image = models.URLField(max_length=100, null=True, blank=True)
+  owner = models.ForeignKey(
+    to='users.User',
+    on_delete=models.CASCADE,
+    related_name='owned_games',
+    null=True
+
+  )
+  def __str__(self):
+    return f'{self.title}'
